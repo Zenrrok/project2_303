c61dbce8447aae63722c94565370064cadae0c97
sretzke <stephanie.retzke@gmail.com>
2015-10-28 19:16:08 -0500

    Update books.h

---
+++ project2_303/project2_303/books.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "date.h" //I'm just assuming
+#include "employee.h"
+#include <queue>
+using namespace std;
+
+class book
+{
+public:
+	//default constructer
+	book();
+	//setters
+	void setName(string a);
+	void setCircStart(date b);
+	void setCircEnd(date c);
+	void addEmployee(employee d);
+	void removeEmployee(employee e);
+	void isArchived();
+
+	//getters
+	string getName();
+	date getCircStart();
+	date getCircEnd();
+	employee getEmployee();
+	bool checkArchived();
+
+private:
+	string name;
+	date circStart;
+	date circEnd;
+	bool archived;
+	queue<employee> plannedEmployeeQ;
+	
+};
