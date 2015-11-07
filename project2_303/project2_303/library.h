cca32ac9860e29e2fd7f7358b370d129c0095d76
sretzke <stephanie.retzke@gmail.com>
2015-11-07 14:11:01 -0600

    Update library.h

---
+++ project2_303/project2_303/library.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "books.h" //I'm just assuming
+#include "date.h"
+
+using namespace std;
+
+class Library
+{
+public:
+	//default constructer
+	Library();
+
+	void addBook(string a);
+	void addEmployee(string b);
+	void circulateBook(string t, date c);
+	void passOn(string ti, date d);
+
+private:
+	string title;
+	date dateToPassBook;
+	List <Book> booksToBeCirc;
+  List <Book> archivedBooks;
+	
+};
